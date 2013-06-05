2317724aa379f2588f7d20d3614883b2fc38a754
Yusuke NAKAYAMA <santa@ubuntu.(none)>
2013-06-05 01:59:43 -0700

    Under making Lexer

--- MarkdownToXML.js
+++ MarkdownToXML.js
@@ -1,4 +1,23 @@
+var index = 0;
+var word = [];
 var Lexer = function(wordLine) {
+	var parts;	
 
+	parts = wordLine.charAt(i);
+	while(parts != null) {
+		match(parts, wordLine, index);
+		index++;
+	}
+
+}
+
+var match = function(parts, wordLine, index) {
+	var Sentence = "";
+	
+
+	switch(parts) {
+	case #:	while(wordLine.charAt(index) != null) {
+			Sentence = Sentence + wordLine.charAt(index);	
+		}
 
 }
